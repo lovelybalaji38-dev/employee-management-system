6e43534cea17f20a6c19a7b5c2d94244758338e0
Gokula Balaji <lovelybalaji38@gmail.com>
2026-04-20 01:02:59 -0700

    Added gitignore ,requirements.txt,settings update

--- ems/settings.py
+++ ems/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3k7rljl(^9xg@wrh+@1^p!3g7r$%-d5n3sgq(0whvux-kvi3&b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,8 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files
 MEDIA_URL = '/media/'
