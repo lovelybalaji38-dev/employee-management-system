2a64b506debdebe67c8973cd6eec0445f1a1bdb9
Gokula Balaji <lovelybalaji38@gmail.com>
2026-04-20 01:10:54 -0700

    Updated settings for production env

--- ems/settings.py
+++ ems/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3k7rljl(^9xg@wrh+@1^p!3g7r$%-d5n3sgq(0whvux-kvi3&b'
+import os
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret")
+
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ['*']
 
